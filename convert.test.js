6f647de3678bb197d9b31dfde4ffa3cc6952c7dd
hz <lebber3131@gmail.com>
2022-12-04 15:23:03 +0100

    commit

--- convert.test.js
+++ convert.test.js
@@ -2,4 +2,7 @@ const convert = require("./convert");
 
 test("Convert an integer to roman numeral", () => {
   expect(convert(1)).toBe("I");
+  expect(convert(12)).toBe("XII");
+  expect(convert(123)).toBe("CXXIII");
+  expect(convert(1234)).toBe("MCCXXXIV");
 });
